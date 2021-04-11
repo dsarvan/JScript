a71219b1bf4f175c5836f262d0dd035521654381
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-04-11 21:29:55 +0530

    script file imported in noteapp.js

---
+++ utils.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+// File: utils.js
+// Name: D.Saravanan
+// Date: 08/04/2021
+// Script file imported in noteapp.js
+
+console.log('utils.js')
+
+const name = 'raman';
+
+const add = function(a, b) {
+    return a + b
+}
+
+module.exports = name
+//module.exports = add
