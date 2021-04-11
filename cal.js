8c9300d76774fd1778c845b05b6c7fe35405a7e5
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-04-11 21:25:30 +0530

    script to print sum values

---
+++ cal.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+// File: cal.js
+// Name: D.Saravanan
+// Script to print sum values
+
+let total = 0, count = 1;
+
+while (count <= 10) {
+    total += count;
+    count += 1;
+}
+
+console.log(total);
